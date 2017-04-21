9c7d13a4ec2011487ea9c4b918309ef26b2dad50
Viet Tran <tdviet@gmail.com>
2017-04-21 13:43:22 +0200

    Update install_gpu.sh

--- install_gpu.sh
+++ install_gpu.sh
@@ -5,20 +5,20 @@
 # Make sure to choose cloud image with UEFI support 
 # https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-uefi1.img
 
-
 # Installing gcc, make and kernel-extra 
 sudo apt-get update
 sudo apt-get -y install gcc make linux-image-extra-virtual
 
 # Instaling NVIDA driver
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/346.96/NVIDIA-Linux-x86_64-346.96.run
-chmod a+x NVIDIA-Linux-x86_64-346.96.run
-sudo ./NVIDIA-Linux-x86_64-346.96.run -a
+wget http://us.download.nvidia.com/tesla/375.51/nvidia-driver-local-repo-ubuntu1404_375.51-1_amd64.deb
+sudo dpkg -i nvidia-driver-local-repo-ubuntu1404_375.51-1_amd64.deb
 
 # Installing CUDA. You can choose only runtime lib (cuda-runtime-version)
 # or all packages  "sudo apt-get install cuda"
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda-runtime-7-5 
 
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda-runtime-8-0
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
